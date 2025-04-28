0c0a3adff27f5d4685efe0b956662ca50e6320c1
Enes Bülkü <ebuelkue@stud.hs-heilbronn.de>
2025-04-28 15:17:42 +0200

    login und registrieung mit datenbank verbunden

--- ss-2025-18.py
+++ ss-2025-18.py
@@ -1,30 +1,18 @@
-"""Webbasierte Systeme - Gruppe 18
-"""
-from calendar import error
-
-# Import benötigter Flask-Module
-from flask import Flask, render_template, g, request, redirect, url_for
-# Import MySQL-Connector
+from flask import Flask, render_template, g, request, redirect, url_for, session
 import mysql.connector
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Import der Verbindungsinformationen zur Datenbank:
-# Variable DB_HOST: Servername des MySQL-Servers
-# Variable DB_USER: Nutzername
-# Variable DB_PASSWORD: Passwort
-# Variable DB_DATABASE: Datenbankname
+# Import der Verbindungsinformationen zur Datenbank
 from db.db_credentials import DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE
 
 app = Flask(__name__)
-
-
-
+app.secret_key = 'dein_geheimes_schluessel'  # Geheimen Schlüssel für Sessions setzen
 
 @app.before_request
 def before_request():
     """ Verbindung zur Datenbank herstellen """
     g.con = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD,
                                     database=DB_DATABASE)
-
 
 @app.teardown_request
 def teardown_request(exception):
@@ -37,9 +25,6 @@ def teardown_request(exception):
 def fahrzeugkatalog():
     return render_template('fahrzeugkatalog.html')
 
-
-
-# Route für die Finanzierungsseite – erlaubt GET (anzeigen) und POST (berechnen)
 @app.route('/finanzierung', methods=['GET', 'POST'])
 def finanzierung():
     rate = None  # Standard: Noch keine Rate berechnet
@@ -65,27 +50,75 @@ def finanzierung():
     # HTML-Template anzeigen und ggf. berechnete Rate übergeben
     return render_template('finanzierung.html', rate=rate)
 
-# Flask-Anwendung starten, nur wenn direkt ausgeführt
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/account')
 def account():
+    if 'user_id' not in session:
+        return redirect(url_for('Login'))  # Wenn der Benutzer nicht eingeloggt ist, zum Login weiterleiten
     return render_template('account.html')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/Login')
+@app.route('/Login', methods=['GET', 'POST'])
 def Login():
+    if request.method == 'POST':
+        email = request.form['email']   # E-Mail aus dem Formular
+        passwort = request.form['passwort']  # Passwort aus dem Formular
+
+        cursor = g.con.cursor()
+
+        # SQL-Abfrage, um nach einem Benutzer mit der angegebenen E-Mail zu suchen
+        sql = "SELECT * FROM users WHERE email = %s"
+        val = (email,)
+        cursor.execute(sql, val)
+        user = cursor.fetchone()
+
+        # Überprüfen, ob der Benutzer existiert und das Passwort korrekt ist
+        if user and check_password_hash(user[4], passwort):  # assuming password is at index 4
+            session['user_id'] = user[0]  # user[0] ist die ID des Benutzers
+            return redirect(url_for('index'))  # Nach dem Login zur Homepage weiterleiten
+        else:
+            return "Benutzername oder Passwort falsch. Bitte versuche es erneut."
+
     return render_template('Login.html')
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.method == 'POST':
+        # Daten aus dem Formular
+        vorname = request.form['firstname']
+        nachname = request.form['lastname']
+        email = request.form['email']
+        passwort = request.form['password']
+
+        cursor = g.con.cursor()
+
+        # SQL-Abfrage zur Überprüfung, ob die E-Mail schon existiert
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            return "E-Mail bereits registriert. Bitte benutze eine andere E-Mail."
+
+        # Passwort vor dem Speichern in der DB hashen
+        hashed_password = generate_password_hash(passwort)
+
+        # SQL-Abfrage zum Einfügen eines neuen Benutzers
+        sql = "INSERT INTO users (vorname, nachname, email, passwort) VALUES (%s, %s, %s, %s)"
+        val = (vorname, nachname, email, hashed_password)  # Gehashtes Passwort hier einfügen
+        cursor.execute(sql, val)
+        g.con.commit()
+
+        return redirect(url_for('Login'))  # Nach der Registrierung zum Login weiterleiten
+
     return render_template('registration.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)  # Entfernt die ID aus der Session, sodass der Benutzer ausgeloggt wird
+    return redirect(url_for('index'))  # Weiterleitung zur Homepage
 
 # Start der Flask-Anwendung
 if __name__ == '__main__':
     app.run(debug=True)
-
