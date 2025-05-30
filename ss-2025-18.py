00bba3745200fb1268b8ced8cbb3d5a9d2387680
Enes Bülkü <ebuelkue@stud.hs-heilbronn.de>
2025-05-30 15:34:50 +0200

    kaufvertrag speichert in der datenbank

--- ss-2025-18.py
+++ ss-2025-18.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g, request, redirect, url_for, session
+from flask import Flask, render_template, g, request, redirect, url_for, session, flash
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 
